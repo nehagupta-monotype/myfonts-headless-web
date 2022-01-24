3c05ad709ee509ee9e405d0a0b5a8311426ae53f
Atul Garg <atul.garg@monotype.com>
2022-01-24 11:11:12 +0530

    added internationalizations

--- next.config.js
+++ next.config.js
@@ -8,8 +8,22 @@ module.exports = {
     return [
       {
         source: '/:path*',
-        destination: 'https://www.myfonts-beta.com/:path*', // Matched parameters can be used in the destination
+        destination: `https://${process.env.SHOPIFY_DOMAIN}/:path*`, // Matched parameters can be used in the destination
       },
     ]
+  },
+  i18n: {
+    locales: ['en-US', 'de-DE',],
+    defaultLocale: 'en-US',
+    // domains: [
+    //   {
+    //     domain: 'myfonts.com',
+    //     defaultLocale: 'en-US',
+    //   },
+    //   {
+    //     domain: 'myfonts.de',
+    //     defaultLocale: 'de-DE',
+    //   },
+    // ],
   }
 }
