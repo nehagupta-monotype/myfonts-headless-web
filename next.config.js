b311d81496b5ec3df9cb31f47e386d76fbd2374a
Atul Garg <atul.garg@monotype.com>
2022-01-24 10:58:15 +0530

    Added rewrites and changed favicon

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  poweredByHeader: false,
   images: {
     domains: ['localhost', 'render.myfonts.net'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://www.myfonts-beta.com/:path*', // Matched parameters can be used in the destination
+      },
+    ]
+  }
 }
